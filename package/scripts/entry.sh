0335d4fff569e549fffdb4c239d8e26e4503f6c2
Dave G <dpg3k@virginia.edu>
2024-12-05 08:47:20 -0500

    Appropriate startup command

--- package/scripts/entry.sh
+++ package/scripts/entry.sh
@@ -4,10 +4,20 @@
 
 # run from here
 cd bin; ./ils-connector-ws \
-   -api $V4_JMRL_API \
-   -apikey $V4_JMRL_API_KEY \
-   -apisecret $V4_JMRL_API_SECRET \
-   -jwtkey $V4_JWT_KEY
+  -authsecret ${AUTH_SHARED_SECRET} \
+  -jwtkey ${JWT_KEY} \
+  -jwtsecret "UNKNOWN" \
+  -pda ${PDA_BASE_URL} \
+  -port ${SERVICE_PORT} \
+  -secretbase "UNKNOWN" \
+  -sirsiclient ${SIRSI_CLIENT_ID} \
+  -sirsilibrary ${SIRSI_LIBRARY} \
+  -sirsipass ${SIRSI_PASSWORD} \
+  -sirsiscript ${SIRSI_SCRIPT_URL} \
+  -sirsiurl ${SIRSI_WEB_SERVICES_BASE} \
+  -sirsiuser ${SIRSI_USER} \
+  -userinfo ${USERINFO_URL} \
+  -virgo ${SEARCH_URL}
 
 return $?
 
