c414d27fbf8a2cc5704b5bf00440aa776155dba5
Lou Foster <louffoster@gmail.com>
2025-03-04 10:56:39 -0500

    simplify renew response

--- cmd/checkouts.go
+++ cmd/checkouts.go
@@ -24,11 +24,6 @@ type renewResponseRec struct {
 	Message       string `json:"message"`
 }
 
-type renewResponse struct {
-	RenewedCount int                `json:"renwed"`
-	Results      []renewResponseRec `json:"results"`
-}
-
 type sirsiRenewResponse struct {
 	CircRecord struct {
 		Fields struct {
@@ -71,49 +66,50 @@ func (svc *serviceContext) renewCheckouts(c *gin.Context) {
 	}
 
 	log.Printf("INFO: user %s requests renew %+v", v4Claims.Barcode, req.Barcodes)
-
-	out := renewResponse{}
+	out := make([]renewResponseRec, 0)
 	for _, renewBC := range req.Barcodes {
-		log.Printf("INFO: issue renew request for %s", renewBC)
-		payload := struct {
-			Barcode string `json:"itemBarcode"`
-		}{
-			Barcode: renewBC,
-		}
-		fields := "circRecord{checkOutDate,dueDate,renewalDate,status,recallDueDate}"
-		rawRenewResp, rawErr := svc.sirsiPost(svc.HTTPClient, fmt.Sprintf("/circulation/circRecord/renew?includeFields=%s", fields), payload)
-		if rawErr != nil {
-			log.Printf("INFO: unable to renew %s: %s", renewBC, rawErr.Message)
-			parsedErr, err := svc.handleSirsiErrorResponse(rawErr)
-			if err != nil {
-				log.Printf("ERROR: unable to parse sirsi failed response: %s", err.Message)
-				out.Results = append(out.Results, renewResponseRec{
-					Barcode: renewBC, Success: false, Message: rawErr.Message,
-				})
-			} else {
-				reason := parsedErr.MessageList[0].Message
-				log.Printf("INFO: renew %s fail reason: %s", renewBC, reason)
-				out.Results = append(out.Results, renewResponseRec{
-					Barcode: renewBC, Success: false, Message: reason,
-				})
-			}
-		} else {
-			log.Printf("INFO: raw renew resp: %s", rawRenewResp)
-			var renewResp sirsiRenewResponse
-			out.RenewedCount++
-			respRec := renewResponseRec{Barcode: renewBC, Success: true}
-			parseErr := json.Unmarshal(rawRenewResp, &renewResp)
-			if parseErr != nil {
-				log.Printf("ERROR: unable to parse renew %s response: %s", renewBC, parseErr.Error())
-			} else {
-				respRec.DueDate = renewResp.CircRecord.Fields.DueDate
-				respRec.RenewDate = renewResp.CircRecord.Fields.RenewalDate
-				respRec.RecallDueDate = renewResp.CircRecord.Fields.RecallDueDate
-				respRec.Status = renewResp.CircRecord.Fields.Status
-			}
-			out.Results = append(out.Results, respRec)
-		}
+		out = append(out, svc.issueReneqRequest(renewBC))
 	}
 
 	c.JSON(http.StatusOK, out)
+}
+
+func (svc *serviceContext) issueReneqRequest(renewBC string) renewResponseRec {
+	log.Printf("INFO: issue renew request for %s", renewBC)
+	payload := struct {
+		Barcode string `json:"itemBarcode"`
+	}{
+		Barcode: renewBC,
+	}
+	fields := "circRecord{checkOutDate,dueDate,renewalDate,status,recallDueDate}"
+	rawRenewResp, rawErr := svc.sirsiPost(svc.HTTPClient, fmt.Sprintf("/circulation/circRecord/renew?includeFields=%s", fields), payload)
+	if rawErr != nil {
+		log.Printf("INFO: unable to renew %s: %s", renewBC, rawErr.Message)
+		parsedErr, err := svc.handleSirsiErrorResponse(rawErr)
+		if err != nil {
+			log.Printf("ERROR: unable to parse sirsi failed response: %s", err.Message)
+			return renewResponseRec{
+				Barcode: renewBC, Success: false, Message: rawErr.Message,
+			}
+		}
+		reason := parsedErr.MessageList[0].Message
+		log.Printf("INFO: renew %s fail reason: %s", renewBC, reason)
+		return renewResponseRec{
+			Barcode: renewBC, Success: false, Message: reason,
+		}
+	}
+
+	var renewResp sirsiRenewResponse
+	respRec := renewResponseRec{Barcode: renewBC, Success: true}
+	parseErr := json.Unmarshal(rawRenewResp, &renewResp)
+	if parseErr != nil {
+		log.Printf("ERROR: unable to parse renew %s response: %s", renewBC, parseErr.Error())
+	} else {
+		respRec.DueDate = renewResp.CircRecord.Fields.DueDate
+		respRec.RenewDate = renewResp.CircRecord.Fields.RenewalDate
+		respRec.RecallDueDate = renewResp.CircRecord.Fields.RecallDueDate
+		respRec.Status = renewResp.CircRecord.Fields.Status
+	}
+	return respRec
+
 }
